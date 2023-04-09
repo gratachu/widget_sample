ed401e8dd860af287cf4322fdb7f2fa1e87f017c
TatsuakiIto <gratachu9@gmail.com>
2023-04-09 18:58:01 +0900

    add: コントローラーを使用し前の画面から次の画面に値を渡す

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,27 +1,73 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  final String text;
+
+  SecondPage({required this.text});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("HelloSecondPage"),
-          backgroundColor: Colors.grey,
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.face, color: Colors.white,), onPressed: (){},
+      appBar: AppBar(
+        title: const Text("HelloSecondPage"),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.face, color: Colors.white,), onPressed: (){},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('前の画面へ戻る'),
             ),
           ],
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('前の画面へ戻る'),
-          ),
-        )
+      ),
     );
   }
 }
+
+// class SecondPage extends StatelessWidget {
+//   final String text;
+//
+//   SecondPage({required this.text});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text("HelloSecondPage"),
+//           backgroundColor: Colors.grey,
+//           centerTitle: true,
+//           actions: <Widget>[
+//             IconButton(
+//               icon: const Icon(Icons.face, color: Colors.white,), onPressed: (){},
+//             ),
+//           ],
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(text),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                 },
+//                 child: const Text('前の画面へ戻る'),
+//               ),
+//             ],
+//           ),
+//         )
+//     );
+//   }
+// }
