9c0a301219685ba2cf5f7c331aa2acf640dc93ba
TatsuakiIto <gratachu9@gmail.com>
2023-04-15 22:49:18 +0900

    remove: extra comment

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -38,39 +38,3 @@ class SecondPage extends StatelessWidget {
     );
   }
 }
-
-// class SecondPage extends StatelessWidget {
-//   final String text;
-//
-//   SecondPage({required this.text});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text("HelloSecondPage"),
-//           backgroundColor: Colors.grey,
-//           centerTitle: true,
-//           actions: <Widget>[
-//             IconButton(
-//               icon: const Icon(Icons.face, color: Colors.white,), onPressed: (){},
-//             ),
-//           ],
-//         ),
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text(text),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   Navigator.pop(context);
-//                 },
-//                 child: const Text('前の画面へ戻る'),
-//               ),
-//             ],
-//           ),
-//         )
-//     );
-//   }
-// }
